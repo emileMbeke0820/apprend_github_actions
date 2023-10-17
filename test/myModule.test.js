95fddda8c72ca27546d7e09fa5284e8a63f79d9f
Emile Essam Mbeke <Emile.EssamMbeke@otto.de>
2023-10-17 20:01:03 +0200

    testing with jest

--- test/myModule.test.js
+++ test/myModule.test.js
@@ -1,5 +1,5 @@
 // myModule.test.js
-const add = require("./chapitre1/myModule.js");
+const add = require("../chapitre1/myModule");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
